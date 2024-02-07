076d88a1e53e99e6d433b4cde8693dc1b2eef419
Rahul Rajeev <rahulrajeev836@gmail.com>
2024-02-07 23:34:57 +0530

    jwt setup in django

--- backend/users/api/serializers.py
+++ backend/users/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import ModelSerializer, ValidationError, Serializer ,EmailField, CharField
 from users.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class UserSerializer(ModelSerializer):
@@ -7,6 +9,17 @@ class UserSerializer(ModelSerializer):
         model = User
         exclude = ('password',)
 
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
 
 class UserRegisterSerializer(ModelSerializer):
     
