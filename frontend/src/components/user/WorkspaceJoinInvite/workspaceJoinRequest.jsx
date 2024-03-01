e56f55d31bae0f4ebd400642b99c47be296e8abc
Rahul Rajeev <rahulrajeev836@gmail.com>
2024-03-01 17:19:52 +0530

    invitation email send with link to join request

--- frontend/src/components/user/WorkspaceJoinInvite/workspaceJoinRequest.jsx
+++ frontend/src/components/user/WorkspaceJoinInvite/workspaceJoinRequest.jsx
@@ -3,13 +3,13 @@ import './workspaceJoinRequest.css'
 
 const WorkspaceJoinRequest = () => {
   return (
-    <div className="card">
-    <span className="card__title">Join Invitation</span>
-    <p className="card__content">You have been invited to join to workspace</p>
-    <div className="card__form">
+    <div className="JoinCard">
+    <span className="JoinCard__title">Join Invitation</span>
+    <p className="JoinCard__content">You have been invited to join to workspace</p>
+    <div className="JoinCard__form">
         
-        <button className="sign-up" style={{width:"200px", backgroundColor:"green"}}>Join</button>
-        <button className="sign-up">Reject</button>
+        <button className="Join" style={{width:"200px", backgroundColor:"green"}}>Join</button>
+        <button className="Reject">Reject</button>
     </div>
 </div>
 
