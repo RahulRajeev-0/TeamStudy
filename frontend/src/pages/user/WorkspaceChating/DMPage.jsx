b339646560dbaa38720ece2880ba85392adde1c9
Rahul Rajeev <rahulrajeev836@gmail.com>
2024-04-02 15:24:22 +0530

    dm messaging complete

--- frontend/src/pages/user/WorkspaceChating/DMPage.jsx
+++ frontend/src/pages/user/WorkspaceChating/DMPage.jsx
@@ -25,10 +25,8 @@ const WorkspaceBody = styled.div`
   display: flex;
   height: 100vh;
   
-  background: url(${backgroundImage});
-  background-position: center;
-  background-size: cover;
-  z-index:-1;
+  background:#282626;
+  
   
   
   
