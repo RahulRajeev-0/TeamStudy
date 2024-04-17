0bf81af9e356fba805269f5c11c09111dbbaaf24
Rahul Rajeev <rahulrajeev836@gmail.com>
2024-04-17 18:43:08 +0530

    video sending in group chat complete

--- frontend/src/pages/user/WorkspaceChating/ChannelPage.jsx
+++ frontend/src/pages/user/WorkspaceChating/ChannelPage.jsx
@@ -4,6 +4,8 @@ import WorkSpaceSideBar from "../../../components/user/WorkSpaceSideBar/WorkSpac
 import Chat from "../../../components/user/chat/GroupChat"
 import styled from 'styled-components'
 
+import backgroundPic from '../../../../../chatBackground.jpeg'
+
 const ChannelPage = () => {
   return (
     <>
@@ -23,7 +25,9 @@ export default ChannelPage;
 const WorkspaceBody = styled.div`
   display: flex;
   height: 100vh;
-  background:#282626;
+  background: rgb(2,0,36);
+background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(198,109,244,1) 100%, rgba(0,212,255,1) 100%);
+  background-size: cover;
 `;
 
 
