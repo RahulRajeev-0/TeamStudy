6939e506ed9961fcaa146acc97ccbd98f576f887
Rahul Rajeev <rahulrajeev836@gmail.com>
2024-04-27 19:36:35 +0530

    made some changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -219,7 +219,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
